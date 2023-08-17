8e0bfb4250da72afadd3df950b2446531c8d2360
Queenkosi <130911572+Queenkosi@users.noreply.github.com>
2023-08-17 09:38:39 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const change = document.getElementById("Ugali na nyama choma (East African)");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("Egusi soup and pounded yam (Nigeria)");
+
+change2.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "orange";
+});
+
+const change3 = document.getElementById("Chicken tagine (Morocco)");
+
+change3.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "green";
+});
+
+const change4 = document.getElementById("Eritrean-food");
+
+change4.addEventListener("click", (event) =>{
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("Bunny chow (South Africa)");
+
+change5.addEventListener("click", (event) =>{
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("Thieboudienne (Senegal)");
+
+change6.addEventListener("click", (event) =>{
+    event.target.style.background = "purple";
+    event.target.style.color = "yellow";
+});
+
+const change7 = document.getElementById("Red red (Ghana)");
+
+change7.addEventListener("click", (event) =>{
+    event.target.style.background = "gold";
+    event.target.style.color = "red";
+});
+
+const change8 = document.getElementById("Injera (Ethiopia)");
+
+change8.addEventListener("click", (event) =>{
+    event.target.style.background = "orange";
+    event.target.style.color = "purple";
+});
